02fe47af193b2952a1adcf861e15180adde41008
sergeyMelentyev <sergey.melentyev@gmail.com>
2017-03-11 16:23:55 +0300

    new version

--- app/src/main/java/com/example/melentyev/sergey/habrahabr/model/Feed.java
+++ app/src/main/java/com/example/melentyev/sergey/habrahabr/model/Feed.java
@@ -6,19 +6,53 @@ public class Feed {
     private String mTitle;
     private String mLinkToFullPost;
     private String mDescription;
+    private String mCategory;
     private String mPubDate;
     private String mImageUrl;
     private String mAuthor;
     private UUID mUUID;
 
-    public Feed(String title, String pubDate, String description,
+    public Feed(String title, String pubDate, String description, String category,
                 String author, String imageUrl, String linkToFullPost) {
         mTitle = title;
         mPubDate = pubDate;
         mDescription = description;
+        mCategory = category;
         mAuthor = author;
         mImageUrl = imageUrl;
         mLinkToFullPost = linkToFullPost;
         mUUID = UUID.randomUUID();
+    }
+
+    public String getAuthor() {
+        return mAuthor;
+    }
+
+    public String getDescription() {
+        return mDescription;
+    }
+
+    public String getImageUrl() {
+        return mImageUrl;
+    }
+
+    public String getLinkToFullPost() {
+        return mLinkToFullPost;
+    }
+
+    public String getPubDate() {
+        return mPubDate;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public String getCategory() {
+        return mCategory;
+    }
+
+    public UUID getUUID() {
+        return mUUID;
     }
 }
--- app/src/main/java/com/example/melentyev/sergey/habrahabr/model/FeedPool.java
+++ app/src/main/java/com/example/melentyev/sergey/habrahabr/model/FeedPool.java
@@ -1,5 +1,38 @@
 package com.example.melentyev.sergey.habrahabr.model;
 
+import android.content.Context;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 public class FeedPool {
-    
+    private static FeedPool sFeedPool;
+    private static List<Feed> mFeedList;
+
+    private FeedPool(Context context) {
+        mFeedList = new ArrayList<>();
+    }
+
+    public static FeedPool getInstanse(Context context) {
+        if (mFeedList == null)
+            return sFeedPool = new FeedPool(context);
+        return sFeedPool;
+    }
+
+    public static List<Feed> getFeedList() {
+        return mFeedList;
+    }
+
+    public static Feed getFeed(UUID uuid) {
+        for (Feed feed : mFeedList)
+            if (feed.getUUID().equals(uuid))
+                return feed;
+        return null;
+    }
+
+    public static void addFeed(Feed feed) {
+        mFeedList.add(feed);
+    }
+
 }
